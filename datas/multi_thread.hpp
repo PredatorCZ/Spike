de3b2c61ba8525a35929dc57c7496e9c4eaf3474
Lukas Cone <lukas.kuzel.ch@gmail.com>
2022-02-02 16:56:09 +0100

    multi_thread add RunThreadedQueueEx

--- datas/multi_thread.hpp
+++ datas/multi_thread.hpp
@@ -1,6 +1,6 @@
 /*  Multi Threading manager
 
-    Copyright 2019-2021 Lukas Cone
+    Copyright 2019-2022 Lukas Cone
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -19,9 +19,10 @@
 #include <algorithm>
 #include <chrono>
 #include <future>
-#include <vector>
 #include <thread>
+#include <vector>
 
+// lmBody(size_t index)
 template <class lmBody> void RunThreadedQueue(size_t numTasks, lmBody &&fc) {
   const size_t numHWThreads = std::thread::hardware_concurrency();
   const size_t numThreads = std::min(numHWThreads, numTasks);
@@ -50,5 +51,68 @@ template <class lmBody> void RunThreadedQueue(size_t numTasks, lmBody &&fc) {
 
   for (auto &wt : workingThreads) {
     wt.wait();
+  }
+}
+
+// lmBody(size_t index)
+template <class lmBody> void RunThreadedQueueEx(size_t numTasks, lmBody &&fc) {
+  const size_t numHWThreads = std::thread::hardware_concurrency();
+  const size_t numThreads = std::min(numHWThreads, numTasks);
+  size_t curTask = 0;
+  std::vector<std::pair<std::future<void>, std::thread>> threads(numThreads);
+
+  auto Invoke = [&](size_t index, std::promise<void> &&state) {
+    try {
+      fc(index);
+    } catch (...) {
+      state.set_exception(std::current_exception());
+    }
+    state.set_value();
+  };
+
+  auto NewTask = [&](auto &data) {
+    auto &[future, thread] = data;
+    std::promise<void> state;
+
+    if (future.valid()) {
+      future.get();
+    }
+
+    if (thread.joinable()) {
+      thread.join();
+    }
+
+    future = state.get_future();
+    thread = std::thread(Invoke, curTask, std::move(state));
+
+    curTask++;
+  };
+
+  for (auto &thread : threads) {
+    NewTask(thread);
+  }
+
+  while (curTask < numTasks) {
+    for (auto &thread : threads) {
+      auto &[future, _] = thread;
+      if (curTask >= numTasks) {
+        break;
+      }
+
+      if ((future.wait_for(std::chrono::milliseconds(2)) ==
+           std::future_status::ready)) {
+        NewTask(thread);
+      }
+    }
+  }
+
+  for (auto &[future, thread] : threads) {
+    if (future.valid()) {
+      future.get();
+    }
+
+    if (thread.joinable()) {
+      thread.join();
+    }
   }
 }
