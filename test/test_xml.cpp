d17b125645e4161e4fcf3e2fe969198158e5bb09
Lukas Cone <lukas.kuzel.ch@gmail.com>
2024-09-22 17:02:32 +0200

    remove legacy xml serializers for reflector

--- test/test_xml.cpp
+++ test/test_xml.cpp
@@ -6,20 +6,6 @@
 #include "spike/reflect/reflector_xml.hpp"
 #include "spike/util/unit_testing.hpp"
 
-int test_reflector_xml_v1() {
-  BinReader mrd("reflected.bin");
-  reflClass rClass2 = {};
-
-  TEST_NOT_CHECK(ReflectorBinUtil::Load(rClass2, mrd));
-
-  pugi::xml_document doc;
-  ReflectorXMLUtil::Save(rClass2, doc, true);
-  reflClass rClass = {};
-  ReflectorXMLUtil::Load(rClass, doc, true);
-
-  return compare_classes(rClass2, rClass);
-}
-
 int test_reflector_xml_v2() {
   BinReader mrd("reflected.bin");
   reflClass rClass2 = {};
@@ -27,9 +13,9 @@ int test_reflector_xml_v2() {
   TEST_NOT_CHECK(ReflectorBinUtil::Load(rClass2, mrd));
 
   pugi::xml_document doc;
-  ReflectorXMLUtil::SaveV2(rClass2, doc, true);
+  ReflectorXMLUtil::Save(rClass2, doc, ReflectorXMLUtil::Flags_ClassNode);
   reflClass rClass = {};
-  ReflectorXMLUtil::LoadV2(rClass, doc, true);
+  ReflectorXMLUtil::Load(rClass, doc, true);
 
   return compare_classes(rClass2, rClass);
 }
@@ -38,8 +24,7 @@ int main() {
   es::SetupWinApiConsole();
   es::print::AddPrinterFunction(es::Print);
 
-  TEST_CASES(int testResult, TEST_FUNC(test_reflector_xml_v1),
-             TEST_FUNC(test_reflector_xml_v2));
+  TEST_CASES(int testResult, TEST_FUNC(test_reflector_xml_v2));
 
   return testResult;
 }
